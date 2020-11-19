b7c24e22d744ca52c236d888243e73d22339e82b
Adarsh Garg <gargadarsh10@gmail.com>
2020-11-19 02:25:25 -0800

    search engine

---
+++ main.py
@@ -0,0 +1,24 @@
+#main.py
+from search_eng import stockSearch, printArticles
+
+
+def NLP():
+	pass
+
+
+
+def main(stock):
+	text = []
+	articles = stockSearch(stock)
+	#---- extract words from each article and place in list----#
+
+	#----- apply NLP or sum -----#
+
+
+	#----- average NLP responses ----#
+	
+
+
+
+
+main()
--- search_eng.py
+++ search_eng.py
@@ -1,1 +1,31 @@
 #search engine using google API
+from googlesearch import search
+
+
+
+def stockSearch(stock, queryNum=30):
+	stock = stock + " price"
+
+	results = []
+	articles = ["barrons", "cnn", "wsj", "cnbc", "yahoo", "businessinsider",
+					"fox", "nytimes", "newyorktimes", "nyse", "reuters", "investing", 
+					"marketwatch", "forbes"]
+
+
+	for res in search(stock, tld="co.in", lang="en", num=queryNum, stop=queryNum, pause=2.0):
+		results.append(res)
+
+	filtered = []
+	for name in articles:
+		for index in range(len(results)):
+			if name in results[index]:
+				filtered.append(results[index])
+	
+	return filtered
+
+
+
+def printArticles(li):
+	for i in li:
+		print((i+1) + ": " + li[i])
+
