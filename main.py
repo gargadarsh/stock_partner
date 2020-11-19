1cb5ac30e2c4d0a8fcd6e87c8bd37d7805b62078
Adarsh Garg <gargadarsh10@gmail.com>
2020-11-19 02:44:31 -0800

    stuff

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 #main.py
 from search_eng import stockSearch, printArticles
 
+'''
+if we do our own NLP implementation then im thinking we do a workflow like:
+	- parse text into list of words 
+		-corner cases: translating apostrophes, punctuations
+
+	-assign weight values to punctuations and certain key words
+		- maybe use neural network implementation??
+
+	-get rid of words like "and", "or" maybe cuz they neutral
+
+	-take avg of weights
+
+	-Problems: there's no way we can assign weights to every key word cuz there are 
+				way too many words that can factor in so idk
+
+
+'''
+
 
 def NLP():
 	pass
 
 
+def parser(str):
+	pass
+
+
 
 def main(stock):
+	articles = []
 	text = []
+
 	articles = stockSearch(stock)
 	#---- extract words from each article and place in list----#
 
